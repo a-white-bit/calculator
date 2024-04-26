dcb3b98af6c61e61fe441739609d71b68f7b2271
soyoung <soyoung.yang94@gmail.com>
2024-04-27 02:39:10 +0900

    lv1-6

--- src/App.java
+++ src/App.java
@@ -6,15 +6,15 @@ public class App {
         Scanner sc = new Scanner(System.in);                // 입력받을 객체(Scanner) 선언
         long num1, num2;                                    // 정수 타입 중 가장 큰 수 표현이 가능한 long을 채택
         char operator;                                      // 사칙연산 문자 1개
-        int resultMaxCount = 10;                            // 연산 저장 개수
+        byte resultMaxCount = 10;                           // 연산 저장 개수
+        byte index = 0;                                     // 연산 배열 인덱스
+        int resultCount = 1;                                // 연산 횟수 카운트
         double[] result = new double[resultMaxCount];       // 연산 결과값 (최대 저장 개수 resultMaxCount개)
         boolean error;                                      // 연산에러 여부
         boolean exit = false;                               // 종료 여부
-        byte index = 0;                                     // 연산 횟수 카운트
 
-        // 사용자가 종료 요청 시 까지 반복 또는,
-        // 최대 연산 횟수 초과 시 까지 반복
-        while (!exit && index < resultMaxCount) {
+        // 사용자가 종료 요청 시 까지 반복
+        while (!exit) {
             // 초기화
             error = false;
 
@@ -40,7 +40,7 @@ public class App {
                 case '*' -> result[index] = num1 * num2;
                 case '/' -> {
                     if (num2 == 0) error = true;
-                    else result[index] = (double)num1 / (double)num2;  // 강제 형변환 필요
+                    else result[index] = (double) num1 / (double) num2;  // 강제 형변환 필요
                 }
             }
 
@@ -48,29 +48,37 @@ public class App {
             // 나중에 예외처리 - 메세지 유연하게 하기
             if (error) {
                 System.out.println("나눗셈 연산에서 두번째 정수가 0이 될 수 없습니다.");
-            }
-            else {
+            } else {
                 System.out.println("--( " + num1 + " " + operator + " " + num2 + " )");
-                System.out.println("--" + (index+1) + ") 연산 결과: " + result[index]);
+                System.out.println("--" + (resultCount) + ") 연산 결과: " + result[index]);
+                // 배열 전부 출력
+                System.out.print("-");
+                for (int i = 0; i <= index; i++)
+                    System.out.print("[" + i + "] " + result[i] + " ");
+                System.out.println();
             }
-
-            // 연산 횟수 초과시 묻지 않고 종료
-            if (index >= resultMaxCount - 1) break;
 
             // 종료 여부 질문
             // '네' 또는 '아니요'가 아닐 시 다시 질문
-            // ...'네' 입력시 처음부터 반복
-            // ...'아니오' 입력시 종료
             while (true) {
                 System.out.print("다시 계산하시겠습니까? (네/아니요) ");
                 String exitQuestion = sc.nextLine();
                 if (exitQuestion.equals("아니요")) {
                     exit = true;
                     break;
-                }
-                else if (exitQuestion.equals("네")) {
-                    // 다음 연산 준비 index += 1
-                    if (!error) index++;
+                } else if (exitQuestion.equals("네")) {
+                    // 다음 연산 준비
+                    if (error) break;
+
+                    // 정상적으로 계산이 완료된 경우 이곳 실행
+                    // 연산 배열이 꽉 찼을 때 else 실행
+                    if (index < result.length - 1) index++;
+                    else {
+                        // 가장 먼저 저장된 결과 삭제
+                        for (int i = 0; i < result.length - 1; i++)
+                            result[i] = result[i + 1];
+                    }
+                    resultCount++;
                     break;
                 }
             }
